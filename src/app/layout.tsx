ea1b9c41ca726d68369acd1f24a899a8193cbb46
Baktybeks <baktybeks@mail.ru>
2025-06-22 14:39:20 +0600

    css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  themeColor: "#4f46e5",
+  // themeColor: "#4f46e5",
 };
 
 export default function RootLayout({
